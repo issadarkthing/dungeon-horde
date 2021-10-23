3ee7d38020439e869d1b77201eeb06af878e15d2
raziman <raziman.terra@gmail.com>
2021-10-23 17:52:25 +0800

    modify bank command

--- src/commands/Bank.ts
+++ src/commands/Bank.ts
@@ -10,12 +10,11 @@ export default class Bank extends UserCommand {
     const user = await this.getUser(msg.author.id);
 
     const embed = new MessageEmbed()
-      .setTitle("User profile")
+      .setTitle("Bank summary")
       .setThumbnail(msg.author.displayAvatarURL())
-      .addField("Name", msg.author.username, true)
       .addField("Bank", `$${user.bank}`, true)
       .addField("Cash", `$${user.balance}`, true)
-      .addField("Total", `$${user.bank + user.balance}`)
+      .addField("Total", `$${user.bank + user.balance}`, true)
 
     msg.channel.send({ embeds: [embed] });
   }
