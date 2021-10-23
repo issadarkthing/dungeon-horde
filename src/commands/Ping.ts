15a33c0ef7c064578a85d19cdec89f9682acaa27
raziman <raziman.terra@gmail.com>
2021-10-23 17:56:05 +0800

    disable ping command

--- src/commands/Ping.ts
+++ src/commands/Ping.ts
@@ -5,6 +5,7 @@ import { UserCommand } from "../structure/UserCommand";
 export default class extends UserCommand {
   name = "ping";
   description = "ping NASA";
+  disable = true;
 
   async exec(msg: Message) {
     const embed = new EmbedTemplate(msg);
